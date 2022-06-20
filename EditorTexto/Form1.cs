56068d7042e21ed6a1e4634e40ac071c32e2df64
Crysthian Zanote Armini <52865098+crysthianzaar@users.noreply.github.com>
2022-06-19 23:51:01 -0300

    Desenvolvimento da tabela Hash e função de espalhamento.

--- EditorTexto/Form1.cs
+++ EditorTexto/Form1.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace EditorTexto
 {
@@ -11,7 +12,7 @@ namespace EditorTexto
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {   
+        {
         }
 
         private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
@@ -21,11 +22,11 @@ namespace EditorTexto
 
         private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
         {
-              openFileDialog1.Filter = "Text Files |*.txt";
-            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            openFileDialog1.Filter = "Text Files |*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
-                
+
             }
         }
 
@@ -85,26 +86,122 @@ namespace EditorTexto
 
         }
 
-        private void validarPalavrasToolStripMenuItem_Click(object sender, EventArgs e)
+        public int HashGerador(string valor)
+        {
+            MD5 md5Hasher = MD5.Create();
+            var hashed = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(valor));
+            var ivalue = BitConverter.ToInt32(hashed, 0);
+            return ivalue;
+            /*int somador = 1;
+            foreach (char letra in valor)
+            {
+                if (letra.Equals('a'))
+                {
+                    somador += 1;
+                }
+                if (letra.Equals('b'))
+                {
+                    somador += 2;
+                }
+                if (letra.Equals('c'))
+                {
+                    somador += 3;
+                }
+                if (letra.Equals('d'))
+                {
+                    somador += 4;
+                }
+                if (letra.Equals('e'))
+                {
+                    somador += 5;
+                }
+                if (letra.Equals('f'))
+                {
+                    somador += 6;
+                }
+                if (letra.Equals('g'))
+                {
+                    somador += 7;
+                }
+                if (letra.Equals('h'))
+                {
+                    somador += 8;
+                }
+                if (letra.Equals('i'))
+                {
+                    somador += 9;
+                }
+                if (letra.Equals('o'))
+                {
+                    somador += 11;
+                }
+            }
+            
+            //int valorHash = valor.GetHashCode();
+            int tamanhoPalavra = valor.Length;
+            
+            int hash =  tamanhoPalavra*somador;
+            return hash; */
+        }
+
+        public static void verificaTexto(RichTextBox box, string text, Color color)
+        {
+            Clipboard.Clear();
+            box.SelectAll();
+            box.Copy();
+            box.Clear();
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = color;
+            box.AppendText(text + " ");
+            box.SelectionColor = box.ForeColor;
+            box.Paste();
+        }
+
+
+        private async void validarPalavrasToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // Carregar o dicionário de palavras
-            var dicionario = File.ReadAllLines("dicionario.txt", Encoding.UTF8);
+            string dicionario = File.ReadAllText("dicionario.txt", Encoding.UTF8);
+            string[] palavras = dicionario.Split(',');
 
             // Transforma em tabela Hash:
             Hashtable tabela = new Hashtable();
 
-            // Carrego a informações digitadas no programa
-            string text = richTextBox1.Text.ToString();
-
-            // Busco e valido através de tabela hash
-
-            // Retorno o richtext sublinhando às palavras que existem no dicionário 
-            richTextBox1.Text = text;
-
-            if (richTextBox1.Text == "oi")
+            foreach (var palavra in palavras)
             {
-                richTextBox1.SelectionColor = Color.Red;
+                try
+                {
+                    int key = HashGerador(palavra);
+                    tabela.Add(key, palavra);
+                }
+                catch
+                {
+
+                }
+            }
+
+                // Carrego a informações digitadas no programa
+                string text = richTextBox1.Text.ToString().ToLower();
+                string[] richTextValues = text.Split(' ');
+                IEnumerable<string> ReverseData = richTextValues.AsEnumerable().Reverse();
+
+                richTextBox1.Clear();
+            // Busco e valido através de tabela hash
+            foreach (var valorAtual in ReverseData)
+                {
+                    int valorKey = HashGerador(valorAtual);
+                    if (tabela.Contains(valorKey))
+                    {
+                       
+                       verificaTexto(richTextBox1,valorAtual,Color.Red);
+                    }
+                    else
+                    {
+                        verificaTexto(richTextBox1, valorAtual, Color.Black);
+                    }
+                }
             }
         }
     }
-}
